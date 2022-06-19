1f859c65693610bc939b610a5e856908157f45fa
omallkait <omalleykait@gmail.com>
2022-06-18 21:46:20 -0400

    C Additional changes

---
+++ app/app.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ui.router'])
+        .config(config)
+        .run(run);
+
+    function config($stateProvider, $urlRouterProvider) {
+        // default route
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'home/index.html',
+                controller: 'HomeIndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'home' }
+            });
+    }
+
+    function run($http, $rootScope, $window) {
+        // add JWT token as default auth header
+        $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
+
+        // update active tab on state change
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            $rootScope.activeTab = toState.data.activeTab;
+        });
+    }
+
+    $(function () {
+        // get JWT token from server
+        $.get('/app/token', function (token) {
+            window.jwtToken = token;
+
+            // bootstrap angular
+            angular.bootstrap(document, ['app']);
+        });
+    });
+})();
---
+++ server.js
@@ -0,0 +1,35 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+var mongo = require('mongoskin');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect(config.localConnectionString)
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
+
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+
+// routes
+app.use('/login', require('./controllers/loginController'));
+app.use('/register', require('./controllers/registerController'));
+app.use('/app', require('./controllers/appController'));
+app.use('/api/users', require('./controllers/api/usersController'));
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
+
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
